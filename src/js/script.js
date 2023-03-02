2e6aaee482a3d2722bc561d3122a5eb742ca1752
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-03-01 21:31:00 -0600

    Add animations disable forms on start and add scoreboard

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,95 @@
-const Player = (name, symbol) => {
+const Player = (name, symbol, card) => {
   let active = false;
+  let score = 0;
 
   const toggleActive = () => {
     active = !active;
+    console.log(card);
+    card.classList.toggle('active');
   };
 
   const isActive = () => active;
+
+  const updateScore = () => {
+    score++;
+  };
+
+  const getScore = () => score;
 
   return {
     name,
     symbol,
     isActive,
     toggleActive,
+    updateScore,
+    getScore,
   };
 };
 
 const gameBoard = (() => {
-  const fieldState = [
+  let fieldArray = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
 
+  const getState = () => fieldArray.map((row) => [...row]);
+
   const updateState = (x, y, symbol) => {
-    fieldState[x][y] = symbol;
+    fieldArray[x][y] = symbol;
   };
 
-  return { fieldState, updateState };
+  const resetState = () => {
+    fieldArray = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+  };
+
+  return { getState, updateState, resetState };
 })();
 
 const gameController = (() => {
-  let isGameActive = false;
-  let player1 = Player('PlaceHolder', 'X');
-  let player2 = Player('PlaceHolder', 'O');
+  let currentRound = 0;
+  let totalRounds = 0;
+  let player1;
+  let player2;
 
-  const checkGameOver = (symbol) => {
+  const onRoundOver = () => {
+    displayController.toggleElements();
+    displayController.updateScoreboard(
+      player1.getScore(),
+      player2.getScore(),
+      `${currentRound} / ${totalRounds}`
+    );
+    displayController.toggleRndBtnDisabled();
+  };
+
+  const onGameOver = (player) => {
+    if (player1 === player) {
+      displayController.updateScoreboard(
+        'Winner!',
+        'Loser...',
+        `${currentRound} / ${totalRounds}`
+      );
+    } else {
+      displayController.updateScoreboard(
+        'Loser...',
+        'Winner!',
+        `${currentRound} / ${totalRounds}`
+      );
+    }
+  };
+
+  const checkRoundOver = (player) => {
+    const symbol = player.symbol;
+
     const checkHorizontal = () => {
       for (let row = 0; row < 3; row++) {
         let hasLine = true;
         for (let col = 0; col < 3; col++) {
-          const currentSymbol = gameBoard.fieldState[row][col];
+          const currentSymbol = gameBoard.getState()[row][col];
           if (currentSymbol !== symbol) {
             hasLine = false;
             break;
@@ -55,7 +105,7 @@ const gameController = (() => {
       for (let col = 0; col < 3; col++) {
         let hasLine = true;
         for (let row = 0; row < 3; row++) {
-          const currentSymbol = gameBoard.fieldState[row][col];
+          const currentSymbol = gameBoard.getState()[row][col];
           if (currentSymbol !== symbol) {
             hasLine = false;
             break;
@@ -70,12 +120,11 @@ const gameController = (() => {
     const checkDiagonals = () => {
       let leftToRightWin = true;
       let rightToLeftWin = true;
-
       for (let i = 0; i < 3; i++) {
-        if (gameBoard.fieldState[i][i] !== symbol) {
+        if (gameBoard.getState()[i][i] !== symbol) {
           leftToRightWin = false;
         }
-        if (gameBoard.fieldState[i][2 - i] !== symbol) {
+        if (gameBoard.getState()[i][2 - i] !== symbol) {
           rightToLeftWin = false;
         }
       }
@@ -83,45 +132,120 @@ const gameController = (() => {
       return leftToRightWin || rightToLeftWin;
     };
 
-    return checkHorizontal() || checkVertical() || checkDiagonals();
+    const checkTie = () => {
+      let isTie = true;
+      gameBoard.getState().forEach((horizontalField) => {
+        horizontalField.forEach((gameSquare) => {
+          if (gameSquare === '') {
+            isTie = false;
+          }
+        });
+      });
+
+      return isTie;
+    };
+
+    if (checkHorizontal() || checkVertical() || checkDiagonals()) {
+      player.updateScore();
+      currentRound++;
+      onRoundOver();
+      console.log(`Round Winner: ${player.name}`);
+      return true;
+    }
+
+    if (checkTie()) {
+      onRoundOver();
+      console.log(`Round Tie`);
+      return true;
+    }
+
+    return false;
+  };
+
+  const checkGameOver = (player) => {
+    if (player.getScore() > totalRounds / 2) {
+      onGameOver(player);
+    }
   };
 
   const takeTurn = (x, y) => {
     if (
-      gameBoard.fieldState[x][y] === player1.symbol ||
-      gameBoard.fieldState[x][y] === player2.symbol ||
-      !isGameActive
+      gameBoard.getState()[x][y] === player1.symbol ||
+      gameBoard.getState()[x][y] === player2.symbol
     )
       return undefined;
 
-    const symbol = player1.isActive() ? player1.symbol : player2.symbol;
+    const activePlayer = player1.isActive() ? player1 : player2;
+    gameBoard.updateState(x, y, activePlayer.symbol);
 
     player1.toggleActive();
     player2.toggleActive();
 
-    gameBoard.updateState(x, y, symbol);
-    if (checkGameOver(symbol)) {
-      console.log(`Winner: ${symbol}`);
-      isGameActive = false;
+    if (checkRoundOver(activePlayer)) {
+      checkGameOver(activePlayer);
     }
-    return symbol;
+
+    return activePlayer;
   };
 
   const init = (nameP1, nameP2, isSymbolX, firstMove, rounds) => {
-    player1 = Player(nameP1, isSymbolX === 'player1' ? 'X' : 'O');
-    player2 = Player(nameP2, isSymbolX === 'player2' ? 'X' : 'O');
+    totalRounds = rounds;
+    player1 = Player(
+      nameP1,
+      isSymbolX === 'player1' ? 'X' : 'O',
+      document.querySelector('.player-1.card')
+    );
+    player2 = Player(
+      nameP2,
+      isSymbolX === 'player2' ? 'X' : 'O',
+      document.querySelector('.player-2.card')
+    );
 
     if (firstMove === 'player1') player1.toggleActive();
     else player2.toggleActive();
-
-    isGameActive = true;
   };
 
   return { takeTurn, init };
 })();
 
-(function displayController() {
+const displayController = (() => {
   const gameContainer = document.querySelector('#game-container');
+  const scoreBoard = document.querySelector('#scoreboard');
+  const resetBtn = document.querySelector('#reset-btn');
+  const startBtn = document.querySelector('#start-btn');
+  const nextRndBtn = document.querySelector('#rnd-btn');
+
+  const toggleResetBtnVisible = () => {
+    resetBtn.classList.toggle('active');
+  };
+
+  const toggleStartBtnVisible = () => {
+    startBtn.classList.toggle('active');
+  };
+
+  const toggleRndBtnVisible = () => {
+    nextRndBtn.classList.toggle('active');
+  };
+
+  const toggleRndBtnDisabled = () => {
+    nextRndBtn.toggleAttribute('disabled');
+  };
+
+  const toggleScoreboardVisible = () => {
+    scoreBoard.classList.toggle('active');
+  };
+
+  const updateScoreboard = (scoreP1, scoreP2, rounds) => {
+    scoreBoard.querySelector('.p1').textContent = scoreP1;
+    scoreBoard.querySelector('.p2').textContent = scoreP2;
+    scoreBoard.querySelector('#rounds-remaining').textContent = rounds;
+  };
+
+  const toggleElements = () => {
+    gameContainer.childNodes.forEach((element) =>
+      element.toggleAttribute('disabled')
+    );
+  };
 
   const buildElement = (elementState) => {
     const element = document.createElement('button');
@@ -132,23 +256,51 @@ const gameController = (() => {
     return element;
   };
 
-  const init = (() => {
-    gameBoard.fieldState.forEach((horizontalArray, x) => {
-      horizontalArray.forEach((gameBoardSquare, y) => {
+  const initBoard = () => {
+    gameBoard.getState().forEach((horizontalField, x) => {
+      horizontalField.forEach((gameBoardSquare, y) => {
         const element = buildElement(gameBoardSquare);
+        element.toggleAttribute('disabled');
         element.addEventListener('click', () => {
-          const symbol = gameController.takeTurn(x, y);
-          if (symbol !== undefined) {
-            element.textContent = symbol;
+          const player = gameController.takeTurn(x, y);
+          if (player !== undefined) {
+            element.textContent = player.symbol;
             element.classList.add('fade-in');
           }
         });
         gameContainer.appendChild(element);
       });
     });
-  })();
+  };
 
-  return { init };
+  const resetDisplay = () => {
+    gameContainer.innerHTML = '';
+    initBoard();
+  };
+
+  const onNextRound = () => {
+    toggleRndBtnDisabled();
+    gameBoard.resetState();
+    resetDisplay();
+    toggleElements();
+  };
+
+  nextRndBtn.addEventListener('click', () => {
+    onNextRound();
+  });
+
+  initBoard();
+
+  return {
+    resetDisplay,
+    toggleResetBtnVisible,
+    toggleStartBtnVisible,
+    toggleRndBtnVisible,
+    toggleRndBtnDisabled,
+    toggleScoreboardVisible,
+    updateScoreboard,
+    toggleElements,
+  };
 })();
 
 (function formController() {
@@ -173,11 +325,44 @@ const gameController = (() => {
     const firstMove = form.elements.firstMove.value;
     const rounds = form.elements.rounds.value;
 
+    displayController.updateScoreboard(0, 0, `0 / ${rounds}`);
+    displayController.toggleScoreboardVisible();
+    displayController.toggleElements();
+    displayController.toggleResetBtnVisible();
+    displayController.toggleStartBtnVisible();
+    if (rounds > 1) {
+      displayController.toggleRndBtnVisible();
+      displayController.toggleRndBtnDisabled();
+    }
+
+    document.querySelector('.player-1.card').toggleAttribute('disabled');
+    document.querySelector('.player-2.card').toggleAttribute('disabled');
+    document.querySelector('.card-title.p1').textContent = nameP1;
+    document.querySelector('.card-title.p2').textContent = nameP2;
+
     gameController.init(nameP1, nameP2, isSymbolX, firstMove, rounds);
+  };
+
+  const onReset = () => {
+    displayController.toggleScoreboardVisible();
+    displayController.toggleResetBtnVisible();
+    displayController.toggleStartBtnVisible();
+
+    document.querySelector('.player-1.card').removeAttribute('disabled');
+    document.querySelector('.player-2.card').removeAttribute('disabled');
+    document.querySelector('.card-title.p1').textContent = 'Player One';
+    document.querySelector('.card-title.p2').textContent = 'Player Two';
+
+    gameBoard.resetState();
+    displayController.resetDisplay();
   };
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     onSubmit();
+  });
+
+  form.addEventListener('reset', (event) => {
+    onReset();
   });
 })();
